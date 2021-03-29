d328ffc0a245db46ad4d8d36baa0f2baa559d8df
qiaochao <752716886@qq.com>
2021-03-29 09:05:56 +0800

    cmake

---
+++ demo5/main.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// 改为选择引入
+#ifdef USE_MYMATH
+    #include "math/MathFunctions.h"
+#else
+    #include <math.h>
+#endif 
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3){
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+
+    // double result = power(base, exponent);
+    // 根据USE_MYMATH是否存在，算出不同的result
+#ifdef USE_MYMATH
+    printf("Now we use our own Math library. \n");
+    double result = power(base, exponent);
+#else
+    printf("Now we use the standard library. \n");
+    double result = pow(base, exponent);
+#endif
+
+
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    return 0;
+}
---
+++ demo7/main.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+// #include "math/MathFunctions.h"
+
+// 改为选择引入
+#ifdef HAVE_POW
+    #include <math.h>
+#else
+    #include "math/MathFunctions.h"
+#endif 
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3){
+
+        // print version info，主版本跨度大，小版本迭代走minor就可以了
+        printf("%s Version %d . %d\n",argv[0],Demo_VERSION_MAJOR,Demo_VERSION_MINOR);
+
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+
+    // double result = power(base, exponent);
+    // 根据USE_MYMATH是否存在，算出不同的result
+#ifdef HAVE_POW
+    printf("Now we use the standard library. \n");
+    double result = pow(base, exponent);
+#else
+    printf("Now we use our own Math library. \n");
+    double result = power(base, exponent);
+
+#endif
+
+
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    return 0;
+}
